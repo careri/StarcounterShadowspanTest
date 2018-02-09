afa9bfb81a2ed8087ff4f84c7fac2b2462e19d4f
bigwad <vadim.senchukov@starcounter.com>
2018-02-09 16:37:03 +0100

    Update server2016_fail.ps1

--- server2016_fail.ps1
+++ server2016_fail.ps1
@@ -108,7 +108,7 @@ try {
     for ($i = 0; $i -le 100; $i++) {
         $time = Get-Date
         Write-Host -ForegroundColor Green "$i, Time = $time"
-        $bytes = [System.BitConverter]::GetBytes($time.Ticks)
+        $timestamp_bytes = [System.BitConverter]::GetBytes($time.Ticks)
         $l = [System.Math]::Max($size, $timestamp_bytes.length)
         $bytes = new-object byte[] $l
         $timestamp_bytes.CopyTo($bytes, 0)
